1136842a43d099733b500e8d15be6dfb04aefd3c
Thomas Mutter <thomas.mutter@nedap.com>
2021-12-10 16:29:06 +0100

    2021-10 with stack

--- advent_of_code_2021/10.swift
+++ advent_of_code_2021/10.swift
@@ -14,12 +14,11 @@ struct NavigationSubsystem {
         lines.map { computeSyntaxErrorScore(in: $0) }.reduce(0,+)
     }
     var lineRepairScore: Int {
-        let sortedScores = lines.map { computeRepairScore(in: $0) }.filter { $0 > 0 }.sorted()
-        return sortedScores[sortedScores.count / 2]
+        lines.map { computeRepairScore(in: $0) }.filter { $0 > 0 }.sorted().middle
     }
     
     private func computeRepairScore(in line: String) -> Int {
-        var unmatchedBrackets = ""
+        var unmatchedBrackets = Stack<Character>()
         let scoringTable: [Character: Int] = [
             ")": 1,
             "]": 2,
@@ -29,15 +28,20 @@ struct NavigationSubsystem {
         
         for bracket in line {
             if "({[<".contains(bracket) {
-                unmatchedBrackets.append(bracket)
-            } else if unmatchedBrackets.last == getMatchingBracket(for: bracket) {
-                let _ = unmatchedBrackets.popLast()
+                unmatchedBrackets.push(bracket)
+            } else if unmatchedBrackets.peek() == getMatchingBracket(for: bracket) {
+                let _ = unmatchedBrackets.pop()
             } else {
                 return 0
             }
         }
         
-        return unmatchedBrackets.reversed().map { scoringTable[getMatchingBracket(for: $0)]! }.reduce(0) { 5*$0 + $1 }
+        var score = 0
+        while let bracket = unmatchedBrackets.pop() {
+            score = 5 * score + scoringTable[getMatchingBracket(for: bracket)]!
+        }
+        
+        return score
     }
     
     private func computeSyntaxErrorScore(in line: String) -> Int {
@@ -87,4 +91,37 @@ struct NavigationSubsystem {
 }
 
 
+extension Array {
+    var middle: Element {
+        self[self.count / 2]
+    }
+}
 
+struct Stack<Element> {
+    private var head: Node<Element>?
+    
+    mutating func push(_ element: Element) {
+        let newNode = Node(next: head, value: element)
+        head = newNode
+    }
+    
+    mutating func pop() -> Element? {
+        let element = head?.value
+        head = head?.next
+        return element
+    }
+    
+    func peek() -> Element? {
+        head?.value
+    }
+
+    private class Node<Element> {
+        var value: Element
+        var next: Node?
+        
+        init(next: Node?, value: Element) {
+            self.next = next
+            self.value = value
+        }
+    }
+}
