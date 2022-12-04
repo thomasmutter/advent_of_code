7b5c4bc6b8e8a41e1431f42ad81471282cb0abb6
thomasmutter <thomas.mutter@nedap.com>
2022-12-04 14:57:27 +0100

    Add overlap' inspired by AoC Reddit

--- 2022/4/day4.hs
+++ 2022/4/day4.hs
@@ -10,7 +10,7 @@ solvePartOne :: [[Char]] -> Int
 solvePartOne = length . filter (uncurry contains . mapTuple readRange . splitFirst ',')
 
 solvePartTwo :: [[Char]] -> Int
-solvePartTwo = length . filter (uncurry overlap . mapTuple readRange . splitFirst ',')
+solvePartTwo = length . filter (uncurry overlap' . mapTuple readRange . splitFirst ',')
 
 readInt :: String -> Int
 readInt = read
@@ -35,7 +35,12 @@ contains x y
     =  fst x <= fst y && snd x >= snd y
     || fst y <= fst x && snd y >= snd x
 
+-- Check if a < c < b or c < a < d for pairs (a,b) and (c,d)
 overlap :: Range -> Range -> Bool
 overlap x y
     =  fst y >= fst x && fst y <= snd x
     || fst x >= fst y && fst x <= snd y
+
+-- Use natural ordering and (b < c) or (d < a) for pairs (a,b) and (c,d) giving no overlap. Then use De Morgan's law to get not (no overlap) = overlap
+overlap' :: Range -> Range -> Bool
+overlap' x y = snd x >= fst y && snd y >= fst x
