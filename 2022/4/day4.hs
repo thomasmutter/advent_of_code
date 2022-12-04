fd86680e697a7d479369ddf8fcbcabb9bc1e383a
thomasmutter <thomas.mutter@nedap.com>
2022-12-04 11:11:48 +0100

    Solve day 4

---
+++ 2022/4/day4.hs
@@ -0,0 +1,41 @@
+type Range = (Int, Int)
+
+main :: IO()
+main = do
+       contents <- readFile "input.txt"
+       print . solvePartOne . lines $ contents
+       print . solvePartTwo . lines $ contents
+
+solvePartOne :: [[Char]] -> Int
+solvePartOne = length . filter (uncurry contains . mapTuple readRange . splitFirst ',')
+
+solvePartTwo :: [[Char]] -> Int
+solvePartTwo = length . filter (uncurry overlap . mapTuple readRange . splitFirst ',')
+
+readInt :: String -> Int
+readInt = read
+
+splitFirst :: Char -> [Char] -> ([Char], [Char])
+splitFirst  _ [] = ([], [])
+splitFirst c (x:xs)
+    | c == x = ([], xs)
+    | otherwise = (x : l, r)
+        where
+            (l, r) = splitFirst c xs
+
+readRange :: String -> Range
+readRange str = (readInt l, readInt r)
+    where (l, r) = splitFirst '-' str
+
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple f (x, y) = (f x, f y)
+
+contains :: Range -> Range -> Bool
+contains x y
+    =  fst x <= fst y && snd x >= snd y
+    || fst y <= fst x && snd y >= snd x
+
+overlap :: Range -> Range -> Bool
+overlap x y
+    =  fst y >= fst x && fst y <= snd x
+    || fst x >= fst y && fst x <= snd y
