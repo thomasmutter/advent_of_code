f581e3a4420dffed64e6b39dfc272c4765cf38f0
thomasmutter <thomas.mutter@nedap.com>
2022-12-06 08:51:40 +0100

    Rename method on day 6

--- 2022/6/day6.hs
+++ 2022/6/day6.hs
@@ -14,16 +14,16 @@ solvePartTwo = detectMessage
 detectMarker :: String -> Int
 detectMarker = go 0
     where
-        go n (w:x:y:z:xs) = if isMarker [w,x,y,z] then n + 4 else go (n+1) (x:y:z:xs)
+        go n (w:x:y:z:xs) = if unique [w,x,y,z] then n + 4 else go (n+1) (x:y:z:xs)
         go _ _ = error "No marker found"
 
 detectMessage :: String -> Int
 detectMessage = go 0
     where
-        go n (a:b:c:d:e:f:g:h:j:i:k:l:m:o:xs) = if isMarker [a,b,c,d,e,f,g,h,j,i,k,l,m,o] then n + 14 else go (n+1) (b:c:d:e:f:g:h:j:i:k:l:m:o:xs)
+        go n (a:b:c:d:e:f:g:h:j:i:k:l:m:o:xs) = if unique [a,b,c,d,e,f,g,h,j,i,k,l,m,o] then n + 14 else go (n+1) (b:c:d:e:f:g:h:j:i:k:l:m:o:xs)
         go _ _ = error "No marker found"
 
 
-isMarker :: [Char] -> Bool
-isMarker [] = True
-isMarker (x:xs) = notElem x xs && isMarker xs
+unique :: [Char] -> Bool
+unique [] = True
+unique (x:xs) = notElem x xs && unique xs
