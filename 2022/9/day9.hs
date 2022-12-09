52540e547996c104b60159ad65d95aea4c989810
thomasmutter <thomas.mutter@nedap.com>
2022-12-09 16:25:12 +0100

    Clean up day 9

--- 2022/9/day9.hs
+++ 2022/9/day9.hs
@@ -1,9 +1,6 @@
-import Data.List ( nub, elemIndices )
+import Data.List ( nub )
 
 type Position = (Int, Int)
-
-type Rope = [Position]
-type Path = [Position]
 
 data Motion = Motion
             { direction :: String
@@ -17,46 +14,24 @@ main = do
        print . solvePartOne . lines $ fileContents
        print . solvePartTwo . lines $ fileContents
 
+-- Should probably use a Set to prevent double storage in the first place
 solvePartOne :: [String] -> Int
-solvePartOne =  length . nub . ((0,0):) . simulateRope ((0,0), (0,0)) . concatMap parseMotion'
+solvePartOne =  length . nub  . simulateRope [(0,0) | _ <- [0..1]] . concatMap parseMotion
 
 solvePartTwo :: [String] -> Int
-solvePartTwo =  length . nub . ((0,0):) . simulateRope' ([(0,0) | _ <- [1..9]], (0,0)) . concatMap parseMotion'
+solvePartTwo =  length . nub . simulateRope [(0,0) | _ <- [0..9]] . concatMap parseMotion
 
-parseMotion :: String -> Motion
-parseMotion str = let parts = words str in Motion { direction = head parts, amount = (read . last) parts }
-
-parseMotion' :: String -> [Motion]
-parseMotion' str = let parts = words str
-                       d = head parts
-                       n = (read . last) parts
+-- This is a hack
+parseMotion :: String -> [Motion]
+parseMotion str = let parts = words str
+                      d = head parts
+                      n = (read . last) parts
                    in go 0 n d where go m n d = if m == n then [] else Motion {direction = d, amount = 1} : go (m+1) n d
 
-simulateRope :: (Position, Position) -> [Motion] -> [Position]
+simulateRope :: [Position] -> [Motion] -> [Position]
 simulateRope _ [] = []
-simulateRope (ot, oh) (m:ms) = let nh = move oh m
-                               in let pos = f ot nh in pos : simulateRope (pos, nh) ms
-
-simulateRope' :: ([Position], Position) -> [Motion] -> [Position]
-simulateRope' _ [] =[]
-simulateRope' (otails, ohead) (m:ms) = let nhead = move ohead m
-                                       in let (ntails, pos) = trail otails nhead in pos : simulateRope' (ntails, nhead) ms
-
-
-
-dbg :: [Position] -> Position -> Motion -> [Position]
-dbg ps p m = fst (trail ps (move p m))
-
-dbgps :: [Position] -> Position -> Motion -> [[Position]]
-dbgps ps h (Motion a b) = go 1
-    where go n = if n > b then [] else dbg ps h (Motion a n) : go (n+1)
-
-allPos :: [Position] -> Position -> [Motion] -> [[Position]]
-allPos ps h [] = []
-allPos ps h (m:ms) = dbgps ps h m ++ allPos (dbg ps h m) (move h m) ms
-
-dodbg :: [Motion] -> String
-dodbg ms = unlines (map drawGrid (allPos [(0,0) | _ <- [1..9]] (0,0) ms))
+simulateRope ps (m:ms) = let nhead = move (head ps) m
+                           in let (ntails, pos) = trail (nhead : tail ps) in pos : simulateRope (nhead : ntails) ms
 
 move :: Position -> Motion -> Position
 move (i, j) (Motion "R" a) = (i + a, j)
@@ -65,31 +40,16 @@ move (i, j) (Motion "U" a) = (i, j + a)
 move (i, j) (Motion "D" a) = (i, j - a)
 move _ _                   = error "Invalid motion"
 
-tailTrail :: Position -> Position -> (Position, [Position])
-tailTrail (i, j) (k, l)
-    | abs dx == 1 && abs dy == 1    = ((i,j), [])
-    | dx /= 0 && dy /= 0    = let pos = (i + signum dx, j + signum dy) in let (p, ps) = tailTrail pos (k, l) in (p, pos : ps)
-    | dx == 0 && abs dy > 1 = let pos = (i, j + signum dy) in let (p, ps) = tailTrail pos (k, l) in (p, pos : ps)
-    | abs dx > 1 && dy == 0 = let pos = (i + signum dx, j) in let (p, ps) = tailTrail pos (k, l) in (p, pos : ps)
-    | otherwise             = ((i,j), [])
-        where (dx, dy) = (k - i, l - j)
-
-f :: Position -> Position -> Position
-f (i, j) (k, l)
+update :: Position -> Position -> Position
+update (i, j) (k, l)
     | abs dx == 1 && abs dy == 1    = (i,j)
-    | dx /= 0 && dy /= 0            = f (i + signum dx, j + signum dy) (k,l)
-    | dx == 0 && abs dy > 1         = f (i, j + signum dy) (k,l)
-    | abs dx > 1 && dy == 0         = f (i + signum dx, j) (k,l)
+    | dx /= 0 && dy /= 0            = update (i + signum dx, j + signum dy) (k,l)
+    | dx == 0 && abs dy > 1         = update (i, j + signum dy) (k,l)
+    | abs dx > 1 && dy == 0         = update (i + signum dx, j) (k,l)
     | otherwise                     = (i,j)
         where (dx, dy) = (k - i, l - j)
 
-trail :: [Position] -> Position -> ([Position], Position)
-trail [] _ = error "Should never reach here"
-trail [p] h = let p' = f p h in ([p'], p')
-trail (p:ps) h = let (mp, seen) = trail ps (f p h) in (f p h : mp, seen)
-
-drawGrid :: [Position] -> String
-drawGrid pos = concat ['\n' : concat [if (x,y) == (0,0) then "s" else if (x, y) `elem` pos then show (head (elemIndices (x,y) pos) + 1) else "." | x <- [-11..25]] | y <- [21, 20..(-5)]]
-
-drawGrid' :: [Position] -> String
-drawGrid' pos = concat ['\n' : concat [if (x, y) `elem` pos then "#" else "." | x <- [-11..25]] | y <- [21, 20..(-5)]]
+trail :: [Position] -> ([Position], Position)
+trail [h, p] = let p' = update p h in ([p'], p')
+trail (h:p:ps) = let (mp, seen) = trail (update p h : ps) in (update p h : mp, seen)
+trail _ = error "Should never reach here"
